83728d6e57de6c7335714f8541f76b4ff7a229c1
Casianus <casianus@gmx.de>
2025-06-15 15:52:30 +0200

    [TASK] Add api

---
+++ Classes/Configuration/Commands.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'storybook:generate-manifest' => [
+        'class' => \MyVendor\MyFluidStorybook\Command\GenerateStorybookManifestCommand::class,
+        // Optional: Add scheduler configuration here if needed in the future
+    ],
+];
